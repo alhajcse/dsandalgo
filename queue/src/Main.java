c5954fc6359461f859310d8ea4d51e938b7b77a6
alhajcse <alhajcse09@gmail.com>
2025-01-29 17:24:49 +0600

    Test sign commit

--- queue/src/Main.java
+++ queue/src/Main.java
@@ -2,8 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-
-
         MyQueue queue=new MyQueue(5);
         queue.enqueue(1);
         queue.enqueue(2);
